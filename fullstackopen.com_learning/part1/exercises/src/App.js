769f369945e030feeb5b41f15e788f7c6dec2bda
JAM-SEVEN <jiangguo2017@gmail.com>
2021-05-21 17:27:19 +0800

    学习更新

--- fullstackopen.com_learning/part1/exercises/src/App.js
+++ fullstackopen.com_learning/part1/exercises/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Author: JAM-SEVEN
+ * @Date: 2021-05-16 11:15:24
+ * @LastEditTime: 2021-05-16 11:58:59
+ * @Description: TO DO
+ */
+import React from 'react'
 
-function App() {
+const course = 'Half Stack application development'
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
+
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{props.course}</h1>
     </div>
-  );
+  )
+  
 }
 
-export default App;
+const Content = () => {
+  return (
+    <div>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
+    </div>
+    
+  )
+  
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      Number of exercises: {props.exercises}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>{props.part} {props.exercise}</div>
+  )
+  
+}
+
+
+
+const App = () => {
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total exercises={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
