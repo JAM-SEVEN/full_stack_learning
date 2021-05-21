e0c04919794c1b61bafaf14345e41f00c92f151b
JAM-SEVEN <jiangguo2017@gmail.com>
2021-05-21 21:16:27 +0800

    20210521_1

---
+++ fullstackopen.com_learning/part1/handout/src/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: JAM-SEVEN
+ * @Date: 2021-05-16 11:15:24
+ * @LastEditTime: 2021-05-16 11:30:29
+ * @Description: TO DO
+ */
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
