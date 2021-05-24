064c9ff0d7a9595606206af1db26b68818846e7c
JAM-SEVEN <jiangguo2017@gmail.com>
2021-05-24 10:54:41 +0800

    学习中20210522

--- fullstackopen.com_learning/part1/handout/src/App.js
+++ fullstackopen.com_learning/part1/handout/src/App.js
@@ -1,7 +1,7 @@
 /*
  * @Author: JAM-SEVEN
  * @Date: 2021-05-16 11:15:24
- * @LastEditTime: 2021-05-21 21:15:30
+ * @LastEditTime: 2021-05-21 21:36:37
  */
 import React, { useState } from 'react'
 
@@ -11,23 +11,30 @@ const App = () => {
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
+  const handleLeft = () => {
+    setAll(allClicks.concat("L"))
     setLeft(left + 1)
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
+  const handleRight = () => {
+    setAll(allClicks.concat("R"))
     setRight(right + 1)
+  }
+
+  const handleZero = () => {
+    setAll(allClicks.concat("0"))
+    setLeft(0)
+    setRight(0)
   }
 
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <button onClick={handleLeft}>left</button>
+      <button onClick={handleZero}>zero</button>
+      <button onClick={handleRight}>right</button>
       {right}
-      <p>{allClicks.join(' ')}</p>
+      <p>{allClicks.join("->")}</p>
     </div>
   )
 }
