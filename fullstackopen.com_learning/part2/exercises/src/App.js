13b35cdac9ac5f7c86ab748adce2bf34f04f0a98
JAM-SEVEN <jiangguo2017@gmail.com>
2021-05-26 17:38:22 +0800

    20210526_2_a_2.1

--- fullstackopen.com_learning/part2/exercises/src/App.js
+++ fullstackopen.com_learning/part2/exercises/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Author: JAM-SEVEN
+ * @Date: 2021-05-16 11:15:24
+ * @LastEditTime: 2021-05-26 17:34:58
+ * @Description: TO DO
+ */
+import React from 'react'
 
-function App() {
+const Header = ({name}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{name}</h1>
     </div>
-  );
+  )
+  
 }
 
-export default App;
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map(part=><Part />)}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      Number of exercises: {props.exercises}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>{props.part} {props.exercise}</div>
+  )
+  
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  ) 
+}
+
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+
+    return <Course course={course} />
+ 
+}
+
+export default App
