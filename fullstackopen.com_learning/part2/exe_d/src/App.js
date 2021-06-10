01823dfe2adaf70bdaaef7ffc3d90c889f958a9e
JAM-SEVEN <jiangguo2017@gmail.com>
2021-06-10 16:35:43 +0800

    part2_exe_d_format

---
+++ fullstackopen.com_learning/part2/exe_d/src/App.js
@@ -0,0 +1,50 @@
+/*
+ * @Author: JAM-SEVEN
+ * @Date: 2021-05-27 14:54:07
+ * @LastEditTime: 2021-06-10 11:32:21
+ * @Description: TO DO
+ */
+
+import React, { useState, useEffect } from 'react'
+import Filter from './components/Filter'
+import Countries from './components/Countries'
+import axios from 'axios'
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filterCountries, setFilter] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log(response.data)
+        setCountries(response.data)
+      })
+    
+  }
+  useEffect(hook, [])
+  
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+    console.log(event.target.value)
+    
+  }
+
+  const handleOnShow = (event) => {
+    console.log(event.target.id)
+    setFilter(event.target.id)
+  }
+
+  return (
+    <div>
+      <h2>Data for countries</h2>
+      <Filter filterCountries={filterCountries} handleFilter={handleFilter}/>
+      <Countries countries={countries} filterCountry={filterCountries} show={handleOnShow}/>
+    </div>
+    
+  )
+}
+
+export default App
